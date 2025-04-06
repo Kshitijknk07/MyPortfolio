c9f94f0559327bda8aff52f8774136887e1393b8
KSHITIJ NARAYAN KULKARNI <knk200407@gmail.com>
2025-04-06 19:09:17 +0530

    NOPE

--- src/components/misc/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as Header } from "./headers.astro";
-export { default as HeaderLink } from "./header-link.astro";
-export { default as FormattedDate } from "./formatted-date.astro";
-export { default as GridBackground } from "./grid-background.astro";
