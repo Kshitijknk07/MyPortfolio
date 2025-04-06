726c36ecdcf5c8f813fb8d1cdacbdc7f43415c09
KSHITIJ NARAYAN KULKARNI <knk200407@gmail.com>
2025-04-06 13:41:25 +0530

    reset

--- next.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    formats: ['image/avif', 'image/webp'],
-    minimumCacheTTL: 60,
-  },
-};
-
-export default nextConfig;
